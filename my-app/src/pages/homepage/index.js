8d6d3138004b8cb30a41f5b4c495cb9b0a04520a
Hannah-C <choihannah2003@gmail.com>
2023-09-16 14:36:34 -0400

    homepage

--- my-app/src/pages/homepage/index.js
+++ my-app/src/pages/homepage/index.js
@@ -5,8 +5,9 @@ import 'tailwindcss/tailwind.css'; // fix config file and then remove this line
 
 const Homepage = ({}) => {
   return(
-    <div>
-      <p className="text-HTN-primary-colour font-bold text-7xl">EYE'm Hungry!</p>
+    <div className="items-center justify-center w-full h-full">
+      <img src="/EYE'm-HUNGRY-logo.png" style={{ "width": "700px", "height": "500px"}}/>
+      {/* <p className="text-HTN-primary-colour font-bold text-7xl">EYE'm Hungry!</p> */}
     </div>
     
   )
